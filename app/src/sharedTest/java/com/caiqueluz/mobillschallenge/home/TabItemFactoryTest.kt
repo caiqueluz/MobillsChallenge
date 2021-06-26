7f817b1b15d04c400762a6adc6c5f82ba92ce90c
caiqueluz <eucaiqueluz@gmail.com>
2021-06-26 17:17:31 -0300

    add tests for TabItemFactory
    Signed-off-by: caiqueluz <eucaiqueluz@gmail.com>

---
+++ app/src/sharedTest/java/com/caiqueluz/mobillschallenge/home/TabItemFactoryTest.kt
@@ -0,0 +1,67 @@
+package com.caiqueluz.mobillschallenge.home
+
+import android.content.Context
+import androidx.test.core.app.ApplicationProvider
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.caiqueluz.mobillschallenge.FirstFragment
+import com.caiqueluz.mobillschallenge.SecondFragment
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class TabItemFactoryTest {
+
+    private val resources = ApplicationProvider.getApplicationContext<Context>().resources
+    private val factory = TabItemFactory(resources)
+
+    private val items = factory.createItems()
+
+    @Test
+    fun whenCreateItemsIsCalled_verifyFirstItemTabNameIsCorrect() {
+        val expected = "Início"
+        val actual = items[0].name
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun whenCreateItemsIsCalled_verifyFirstItemTabFragmentIsCorrect() {
+        val expected = HomeFragment::class.java
+        val actual = items[0].fragment::class.java
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun whenCreateItemsIsCalled_verifySecondItemTabNameIsCorrect() {
+        val expected = "Despesas"
+        val actual = items[1].name
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun whenCreateItemsIsCalled_verifySecondItemTabFragmentIsCorrect() {
+        val expected = FirstFragment::class.java
+        val actual = items[1].fragment::class.java
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun whenCreateItemsIsCalled_verifyThirdItemTabNameIsCorrect() {
+        val expected = "Receitas"
+        val actual = items[2].name
+
+        assertEquals(expected, actual)
+    }
+
+    @Test
+    fun whenCreateItemsIsCalled_verifyThirdItemTabFragmentIsCorrect() {
+        val expected = SecondFragment::class.java
+        val actual = items[2].fragment::class.java
+
+        assertEquals(expected, actual)
+    }
+}
