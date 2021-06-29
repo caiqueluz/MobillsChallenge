131b006465e632b8f45e7dce90012b2174491162
caiqueluz <eucaiqueluz@gmail.com>
2021-06-29 13:54:28 -0300

    add tests for TransactionViewModel
    Signed-off-by: caiqueluz <eucaiqueluz@gmail.com>

---
+++ app/src/test/java/com/caiqueluz/mobillschallenge/transaction/TransactionViewModelTest.kt
@@ -0,0 +1,31 @@
+package com.caiqueluz.mobillschallenge.transaction
+
+import androidx.lifecycle.Observer
+import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.caiqueluz.mobillschallenge.ConcurrentTest
+import com.caiqueluz.mobillschallenge.transaction.main.TransactionFlow
+import com.caiqueluz.mobillschallenge.transaction.main.TransactionViewModel
+import com.nhaarman.mockitokotlin2.eq
+import com.nhaarman.mockitokotlin2.mock
+import com.nhaarman.mockitokotlin2.verify
+import kotlinx.coroutines.ExperimentalCoroutinesApi
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@ExperimentalCoroutinesApi
+@RunWith(AndroidJUnit4::class)
+class TransactionViewModelTest : ConcurrentTest() {
+
+    private val viewModel = TransactionViewModel()
+
+    @Test
+    fun whenOnFlowReceivedIsCalled_verifyObserverValueIsCorrect() {
+        val observer = mock<Observer<TransactionFlow>>()
+        viewModel.navigate.observeForever(observer)
+
+        val mockFlow = mock<TransactionFlow>()
+        viewModel.onFlowReceived(mockFlow)
+
+        verify(observer).onChanged(eq(mockFlow))
+    }
+}
